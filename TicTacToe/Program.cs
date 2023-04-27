6095d5798f0abb1f1493928d88b7f354e404bf8f
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-04-27 08:53:09 -0500

    Initializing variables

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace TicTacToe
+﻿using System;
+using System.Threading;
+
+namespace TicTacToe
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static char[] spaces = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
